40568126d834165a99685b165d346ca49c30a3b6
baraneetharan <baraneetharan@live.com>
2018-08-13 16:40:48 +0530

    Update BookCategory.java

--- src/main/java/com/psg/glms/model/BookCategory.java
+++ src/main/java/com/psg/glms/model/BookCategory.java
@@ -4,7 +4,11 @@ import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "book_category")
 public class BookCategory {
 
